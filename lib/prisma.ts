09eeddce12d0680595b6963054b376fc3bcdac3c
rozzy <rosekaremeri@gmail.com>
2024-06-28 00:40:29 +0300

    create prisma client instance

---
+++ lib/prisma.ts
@@ -0,0 +1,17 @@
+import { PrismaClient } from '@prisma/client';
+
+let prisma: PrismaClient;
+
+if (process.env.NODE_ENV === 'production') {
+  prisma = new PrismaClient();
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
+  }
+  prisma = global.prisma;
+}
+
+export default prisma;
+
+//whenever you need access to your database you can import the prisma instance into the file where it's needed.
+
