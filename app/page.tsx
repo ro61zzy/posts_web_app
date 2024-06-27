552981cb3c35ca95a5f5ba30e39c2a193dc60169
rozzy <rosekaremeri@gmail.com>
2024-06-28 00:53:46 +0300

    create fn to fetch from db

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,27 @@
-import Image from "next/image";
 import styles from "./page.module.css";
 import prisma from '../lib/prisma'; //instance will be your interface to the database when you want to read and write data in it
 
-export default function Home() {
+
+async function getPosts() {
+  const posts = await prisma.post.findMany({
+    where: {published: true},
+    include: {
+      author:{
+        select: {name: true}
+      }
+    }
+  })
+}
+
+
+export default async function Home() {
+
+  const posts = await getPosts()
+  console.log({posts})
   return (
     <main className={styles.main}>
      <h1>Feed</h1>
-     
+
     </main>
   );
 }
